61f7e789baf6c6c0cb2bd24651b447a75db23923
Hernán Valencia <hernanadrianv17@gmail.com>
2025-06-10 06:05:22 -0500

    ✨ feat: adding main screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1>Ketzai Test</h1>
+    <div className="flex flex-col space-y-5 justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">Ketzai Test</h1>
+      <Button>See table</Button>
     </div>
   );
 }
